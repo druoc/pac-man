1240ec8038cb03053bb654c23c5f6ac9d00ef99a
Dave <d.ruocco1987@googlemail.com>
2021-10-22 15:55:26 +0100

    Finished UI, implemented X axis movement

--- pacmanscript.js
+++ pacmanscript.js
@@ -1,6 +1,7 @@
 const width = 28;
 const grid = document.querySelector('.grid');
 const scoreDisplay = document.querySelector('#score');
+let squares = [];
 
 //28 * 28 = 784
   // 0 - pac-dots
@@ -39,3 +40,57 @@ const scoreDisplay = document.querySelector('#score');
     1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
 ]
+
+//create the game board
+function createBoard() {
+  for (let i = 0; i < layout.length; i++) {
+    //create squares
+    const square = document.createElement('div');
+    //render squares in grid
+    grid.appendChild(square);
+    //put square in squares array
+    squares.push(square);
+
+    if (layout[i] === 0) {
+      squares[i].classList.add('pac-dot');
+    } else if (layout[i] === 1) {
+      squares[i].classList.add('wall');
+    } else if (layout[i] === 3) {
+      squares[i].classList.add('power-pellet');
+    }
+  }
+}
+
+createBoard();
+
+//starting position of pacman
+let pacManCurrentIndex = 500;
+squares[pacManCurrentIndex].classList.add('pacman');
+
+//keystroke event listeners
+function control(e) {
+  squares[pacManCurrentIndex].classList.remove('pacman');
+    switch(e.keyCode) {
+      case 40:
+      console.log('pressed down')
+      if (pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width;
+      break
+
+      case 38:
+      console.log('pressed up')
+      break
+
+      case 37: 
+      console.log('pressed left')
+      if (pacManCurrentIndex % width !== 0) pacManCurrentIndex -=1;
+      break
+
+      case 39:
+      console.log('pressed right')
+      if (pacManCurrentIndex % width !== 0) pacManCurrentIndex +=1;
+      break
+  }
+  squares[pacManCurrentIndex].classList.add('pacman');
+} 
+
+document.addEventListener('keyup', control);
